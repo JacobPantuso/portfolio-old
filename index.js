a2345ec1e61ca5d8ab85d6d956413ad74b317bf0
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-15 16:12:14 -0500

    Finalized dark mode changes

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ function toggileMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
+function restoreTheme() {
+    // disable animations on page load
+    document.body.classList.add('no-transition');
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark');
+        document.getElementById('theme-icon').classList.remove("uil-moon");
+        document.getElementById('theme-icon').classList.add("uil-sun");
+    } else {
+        document.body.classList.remove('dark');
+        document.getElementById('theme-icon').classList.remove("uil-sun");
+        document.getElementById('theme-icon').classList.add("uil-moon");
+    }
+}
+
 function toggleTheme() {
+    // enable animations
+    document.body.classList.remove('no-transition');
     if (document.body.classList.contains('dark')) {
         document.body.classList.remove('dark');
         document.getElementById('theme-icon').classList.remove("uil-sun");
