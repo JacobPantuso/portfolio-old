508954a0dcf5a907f12ac2d17094e1af0aed72f7
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-15 16:00:01 -0500

    Added dark-light toggle and top-right

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function toggileMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+function toggleTheme() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.getElementById('theme-icon').classList.remove("uil-sun");
+        document.getElementById('theme-icon').classList.add("uil-moon");
+    } else {
+        // animate the transition to dark
+        document.body.classList.add('dark');
+        document.getElementById('theme-icon').classList.remove("uil-moon");
+        document.getElementById('theme-icon').classList.add("uil-sun");
+    }
+    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+}
